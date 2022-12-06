3c135301e9cac58cf2527c0b92121ba48a04162c
Ben Bowen <ca.longbeach.logan@gmail.com>
2022-12-06 10:46:24 -0700

    changes

--- Audit.src/Infra.VendorData.Driver/DataRetriever/IVendorDataRetrieval.cs
+++
@@ -1,8 +0,0 @@
-using AuditCore;
-
-namespace Infrastructure.VendorData.Driver;
-
-public interface IVendorDataRetrieval
-{
-   IEnumerable<VendorRecord> VendorData();
-}
--- Audit.src/Infra.VendorData.Driver/LeadItem.cs
+++ Audit.src/Infra.VendorData.Driver/LeadItem.cs
@@ -1,18 +1,22 @@
-namespace AuditCore;
+using AuditCore;
+namespace Infrastructure.VendorData.Driver;
 
 public class LeadItem : ILeadItem
 {
-   public LeadItem(VendorRecord vendor, Region callOrigin, DateTime dateTime, PhoneNumber phoneNumber)
+   public LeadItem(VendorRecord vendor, Region callOrigin, DateTime dateTime, PhoneNumber phoneNumber, TimeSpan duration)
    {
       Vendor = vendor;
       CallerRegion = callOrigin;
       DateTime = dateTime;
       PhoneNumber = phoneNumber;
+      CallDuration = duration;
    }
    public VendorRecord Vendor { get; set; }
    public Region CallerRegion { get; set; }
    public DateTime DateTime { get; set; }
    public PhoneNumber PhoneNumber { get; set; }
+   public TimeSpan CallDuration { get; set; }
    public bool Billability { get; set; }
+
 
 }
