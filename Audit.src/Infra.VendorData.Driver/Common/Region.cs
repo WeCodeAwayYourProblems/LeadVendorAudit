0b5fe472e684bad6c2b921e168855c20b2a61e2c
Ben Bowen <ca.longbeach.logan@gmail.com>
2022-12-07 08:39:57 -0700

    Moved this Region class from Core in order to create abstractions from the Core

---
+++ Audit.src/Infra.VendorData.Driver/Common/Region.cs
@@ -0,0 +1,30 @@
+using AuditCore;
+
+namespace Infrastructure.VendorData.Driver;
+public class Region : IRegion
+{
+   public Region(string state, string city, string zip) : this(state, city)
+   {
+      int conversion;
+      if (zip.Length != 5 || !int.TryParse(zip, out conversion))
+         throw new ArgumentException($"Parameter {nameof(zip)} must be a 5-digit zip code without any additional characters.");
+      else
+      {
+         ZipCode = conversion;
+         ZipString = zip;
+      }
+   }
+   public Region(string state, string city) : this(city)
+   {
+      State = state;
+   }
+   public Region(string city)
+   {
+      City = city;
+   }
+   public string? State { get; set; }
+   public string? ZipString { get; set; }
+   public string City { get; set; }
+   public int ZipCode { get; }
+
+}
