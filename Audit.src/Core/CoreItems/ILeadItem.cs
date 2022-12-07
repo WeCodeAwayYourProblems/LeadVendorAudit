68400c751e435b8527bd5be3df7c225b46181c5c
Ben Bowen <ca.longbeach.logan@gmail.com>
2022-12-07 08:40:46 -0700

    Added a level of abstraction away from the Core so that it doesn't have to deal with items concerning the Infrastructure layer.

--- Audit.src/Core/CoreItems/ILeadItem.cs
+++ Audit.src/Core/CoreItems/ILeadItem.cs
@@ -2,7 +2,7 @@ namespace AuditCore;
 public interface ILeadItem
 {
    public VendorRecord Vendor { get; set; }
-   public Region CallerRegion { get; set; }
+   public IRegion CallerRegion { get; set; }
    public DateTime DateTime { get; set; }
    public IPhoneNumber PhoneNumber { get; set; }
    public bool Billability { get; set; }
