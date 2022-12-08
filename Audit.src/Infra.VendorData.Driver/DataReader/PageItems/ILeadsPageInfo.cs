f768ed57222e2fd2b3e116f3e24d56760e4c15bd
Ben Bowen <ca.longbeach.logan@gmail.com>
2022-12-08 07:56:25 -0700

    Moved page items folder because they need to be accessed by the DataReader folder items and the DataWriter folder items.

--- Audit.src/Infra.VendorData.Driver/DataReader/PageItems/ILeadsPageInfo.cs
+++
@@ -1,9 +0,0 @@
-using OpenQA.Selenium;
-
-namespace Infrastructure.VendorData.Driver;
-public interface ILeadsPageInfo
-{
-   public By LeadsElement { get; set; }
-   public By NextPageButton { get; set; }
-   public string NextPageUrl { get; set; }
-}
--- Audit.src/Infra.VendorData.Driver/DataReader/PageItems/IPageItems.cs
+++
@@ -1,8 +0,0 @@
-namespace Infrastructure.VendorData.Driver
-{
-   public interface IPageItems
-   {
-      public LoginPage LoginPage { get; }
-      public LeadsPage LeadsPage { get; }
-   }
-}
