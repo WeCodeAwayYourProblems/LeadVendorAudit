993ebadade8e3ab43a9ecec6ccc4140557ae09c4
Ben Bowen <ca.longbeach.logan@gmail.com>
2023-03-31 13:24:14 -0600

    Region constructors were added to allow for country, city, and state differentiations in Region objects requiring additional information about that specific region. Conversions and data checks for integer and string inputs as zipcodes were also added

--- Audit.src/Infrastructure/Common/Region.cs
+++ Audit.src/Infrastructure/Common/Region.cs
@@ -3,7 +3,38 @@ using AuditCore;
 namespace AuditInfrastructure;
 public class Region : IRegion
 {
-   public Region(string state, string city, string zip) : this(state, city)
+   // Constructors
+   // Zip as integer ctors
+   public Region(string country, string state, string city, int zip) : this(state, city, zip)
+   { Country = country; }
+   public Region(string state, string city, int zip) : this(city, zip)
+   { State = state; }
+   public Region(string city, int zip) : this(zip)
+   { City = city; }
+   public Region(int zip)
+      => ValidateIntZipInput(zip);
+
+   // Zip as string ctors
+   public Region(string country, string state, string city, string zip) : this(state, city, zip)
+   { Country = country; }
+   public Region(string state, string city, string zip) : this(city, zip)
+   { State = state; }
+   public Region(string city, string zip) : this(zip)
+   { City = city; }
+   public Region(string zip)
+      => ConvertZipStringToInt(zip);
+
+   // Public attributes
+   public string? Country { get; set; }
+   public string? State { get; set; }
+   public string? ZipString { get; set; }
+   public string? City { get; set; }
+   public int ZipCode { get; set; }
+
+   // Private attributes -- zip code validation and converters
+   private void ValidateIntZipInput(int zip)
+      => ConvertZipStringToInt($"{zip}");
+   private void ConvertZipStringToInt(string zip)
    {
       int conversion;
       if (zip.Length != 5 || !int.TryParse(zip, out conversion))
@@ -14,17 +45,4 @@ public class Region : IRegion
          ZipString = zip;
       }
    }
-   public Region(string state, string city) : this(city)
-   {
-      State = state;
-   }
-   public Region(string city)
-   {
-      City = city;
-   }
-   public string? State { get; set; }
-   public string? ZipString { get; set; }
-   public string City { get; set; }
-   public int ZipCode { get; }
-
 }
