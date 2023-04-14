3e0b0c97f647e9f2270f3ae337cc86c3459fe967
Ben Bowen <ca.longbeach.logan@gmail.com>
2023-04-14 11:41:16 -0600

    Added structure for the presentation layer, including a helper class that facilitates the cli

---
+++ Audit.src/Presentation/HelperClass.cs
@@ -0,0 +1,9 @@
+namespace CLIInterface;
+
+public class HelperClass : Helper
+{
+   public IEnumerable<string> VendorOptions(string fullPath)
+   {
+
+   }
+}
---
+++ Audit.src/Presentation/Initiator.cs
@@ -0,0 +1,16 @@
+﻿using CliHelper;
+namespace CLIInterface;
+
+static class Initiator
+{
+   static void Main()
+   {
+      HelperClass Init = new();
+
+      // TODO: Retrieve all possible vendors from private list
+      // TODO: Inform the user of all possible vendors
+      // TODO: Ask the user which vendor they want to audit
+      // TODO: Ask the user which date range they would like to audit
+      // TODO: Instantiate an AppLayer object that contains all the necessary information
+   }
+}
