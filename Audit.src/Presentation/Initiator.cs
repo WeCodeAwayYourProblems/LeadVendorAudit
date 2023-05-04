bdf6fac33bc57e4024c58970ff58bc272295ba31
Ben Bowen <ca.longbeach.logan@gmail.com>
2023-05-04 08:56:54 -0600

    Initiator changed.

--- Audit.src/Presentation/Initiator.cs
+++ Audit.src/Presentation/Initiator.cs
@@ -5,12 +5,12 @@ static class Initiator
 {
    static void Main()
    {
-      HelperClass Init = new();
+      HelperClass Helper = new();
 
       // TODO: Retrieve all possible vendors from App Layer
       // TODO: Inform the user of all possible vendors
       // TODO: Ask the user which vendor they want to audit
       // TODO: Ask the user which date range they would like to audit
-      // TODO: Instantiate an AppLayer object that contains all the necessary information
+      // TODO: Instantiate an AppLayer request object that contains all the necessary information
    }
 }
